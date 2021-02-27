a653b9564699ebbb658b87bd2233d8a75b2ec070
Guilherme Paes <guilhermep98@gmail.com>
2021-02-27 19:41:14 -0300

    Added company entity

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -1,1 +1,35 @@
-export class Company {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Company {
+  @PrimaryGeneratedColumn()
+  private id: number;
+
+  @Column()
+  private cnpj: string;
+
+  @Column()
+  private sellRate: number;
+
+  @Column()
+  private name: string;
+
+  @Column()
+  private companyName: string;
+
+  @CreateDateColumn()
+  private created: Date;
+
+  @Column({ default: false })
+  private deleted: boolean;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  private deletedAt: Date;
+
+}
