55ba3a8ee99438b6456b7d3ccbea57a124486dfa
Guilherme Paes <guilhermep98@gmail.com>
2021-02-28 10:34:41 -0300

    / redirects to /docs

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'This is the Aço² API go for https://api-chain.herokuapp.com/docs/ to see the specs';
   }
 }
